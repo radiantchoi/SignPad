2f8a555d6682588741d2fb5ed1b9e92ec6ab173a
Gordon Choi <thefirate@gmail.com>
2023-06-04 19:09:11 +0900

    Initial Commit

---
+++ SignPad/SignPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignPadApp.swift
+//  SignPad
+//
+//  Created by Gordon Choi on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct SignPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
